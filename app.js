dfb939afcd41b13372afa639eff77d8e76a27758
Soufian ELHABIB <elhabib.soufian@gmail.com>
2020-07-16 20:46:32 +0100

    Fix some problems in Menu collapse/Time line is done

--- app.js
+++ app.js
@@ -487,3 +487,27 @@ $('.navTrigger').click(function() {
 	$('#mainListDiv').toggleClass('show_list');
 	$('#mainListDiv').fadeIn();
 });
+
+
+        $('#year').text(new Date().getFullYear());
+        $('body').scrollspy({ target: '#main-nav' });
+
+    // Add smooth scrolling
+    $('#main-nav a').on('click', function (e) {
+      // Check for a hash value
+      if (this.hash !== '') {
+        // Prevent default behavior
+        e.preventDefault();
+
+        // Store hash
+        const hash = this.hash;
+
+        // Animate smooth scroll
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function () {
+          // Add hash to URL after scroll
+          window.location.hash = hash;
+        });
+      }
+    });
